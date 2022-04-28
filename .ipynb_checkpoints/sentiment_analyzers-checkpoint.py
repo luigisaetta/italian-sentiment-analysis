8708d33f3db5efe37c5b77d4e92f3e9bd695f85a
luigisaetta <luigi.saetta@gmail.com>
2022-04-28 14:27:12 +0000

    added gpu support

--- .ipynb_checkpoints/sentiment_analyzers-checkpoint.py
+++ .ipynb_checkpoints/sentiment_analyzers-checkpoint.py
@@ -1,14 +1,14 @@
 import torch
 from torch import nn
 
-# HuggingFace transformers (availale in OCI DS conda nlp env)
+# HuggingFace transformers (available in OCI DS conda nlp env)
 # see: https://github.com/huggingface/transformers
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 #
 # a more flexible version
 #
-class MultiSentimentAnalyzer:
+class MultiLanguageSentimentAnalyzer:
     # load the tokenizer and transformer
     def __init__(self, MODEL_NAME, labels):
 
@@ -25,24 +25,33 @@ class MultiSentimentAnalyzer:
         # the list of defined labels (and therefore we will have 5 scores)
         # this is changed from ITA class
         self._LABELS = labels
-
+        
+        # to enable inference on GPU
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
         print("Loading model...")
         self.tokenizer = AutoTokenizer.from_pretrained(self._MODEL_NAME)
         # Load the model
         self.model = AutoModelForSequenceClassification.from_pretrained(
-            self._MODEL_NAME
-        )
+            self._MODEL_NAME).to(device)
 
         print("Model loading completed!")
-
+        
+    def get_device(self):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        return device 
+    
     #
     # does the scoring on a single sentence a time
     #
     def score(self, input_sentence):
         # encode the sentence and create the input tensor (in PyTorch format)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
         input_ids = self.tokenizer(
             input_sentence, add_special_tokens=True, return_tensors="pt"
-        )["input_ids"]
+        )["input_ids"].to(device)
 
         # output from tokenizer is already a tensor
 
@@ -73,10 +82,11 @@ class MultiSentimentAnalyzer:
     def batch_score(self, input_sentences):
         # encode the sentence and create the input tensor (in PyTorch format)
         # requires padding
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
         tokens = self.tokenizer(
             input_sentences, add_special_tokens=True, padding=True,
-            return_tensors="pt"
-        )
+            return_tensors="pt").to(device)
 
         # output from tokenizer is already a tensor
         
@@ -87,7 +97,7 @@ class MultiSentimentAnalyzer:
         
         proba = nn.functional.softmax(logits, dim=1)
         
-        return proba
+        return proba.cpu()
     
     # only to format output
     def format_scores(self, scores):
